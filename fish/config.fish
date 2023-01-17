38bc3f3d7b522c8fca224a1202b1faa2467a01e7
fjij <30779570+fjij@users.noreply.github.com>
2023-01-16 22:33:14 -0500

    Add homebrew shellenv to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,9 @@ set -gx CLICOLOR 1
 set -gx fish_greeting
 fish_config prompt choose arrow
 fish_add_path ~/bin
+if test -e /opt/homebrew/bin/brew
+    eval (/opt/homebrew/bin/brew shellenv)
+end
+if test -e /home/linuxbrew/.linuxbrew/bin/brew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
