aa509f472b7501d8b18bebec86dc73cdbfdeb658
fjij <30779570+fjij@users.noreply.github.com>
2023-01-24 16:39:24 -0500

    Use lazy.nvim

--- nvim/after/plugin/lsp.lua
+++
@@ -1,9 +0,0 @@
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
-
-lsp.set_preferences({
-  set_lsp_keymaps = true,
-  manage_nvim_cmp = true,
-})
-
-lsp.setup()
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,48 +1,88 @@
-vim.api.nvim_create_user_command('GitClonePacker', function()
-  local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-  local packer_url = 'https://github.com/wbthomason/packer.nvim'
-  vim.fn.system({'git', 'clone', '--depth', '1', packer_url, install_path})
-  vim.cmd('quitall')
-end, {})
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-local ok, packer = pcall(require, 'packer')
-if not ok then return end
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
 
-return packer.startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+vim.opt.rtp:prepend(lazypath)
 
-    use 'tpope/vim-sensible'
-    use 'tpope/vim-fugitive'
-    use 'tpope/vim-surround'
-    use {
-        'rose-pine/neovim',
-        as = 'rose-pine',
+require("lazy").setup({
+    "tpope/vim-sensible",
+    "tpope/vim-fugitive",
+    "tpope/vim-surround",
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
         config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
-    }
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = { 'nvim-lua/plenary.nvim' },
-    }
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        requires = {
-            'neovim/nvim-lspconfig',
-            'williamboman/mason.nvim',
-            'williamboman/mason-lspconfig.nvim',
+            vim.cmd("colorscheme rose-pine")
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        keys = {
+            { "<C-p>", ":Telescope find_files<cr>", desc = "Find files" },
+            { "<leader>ps", ":Telescope live_grep<cr>", desc = "Live grep" },
+        },
+        opts = {
+            defaults = { mappings = { i = { ["<esc>"] = require("telescope.actions").close } } },
+        },
+    },
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
 
-            'hrsh7th/nvim-cmp',
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
-            'saadparwaiz1/cmp_luasnip',
-            'hrsh7th/cmp-nvim-lua',
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-nvim-lua",
 
-            'L3MON4D3/LuaSnip',
-            'rafamadriz/friendly-snippets',
-        }
-    }
-    use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
-end)
+            "L3MON4D3/LuaSnip",
+            "rafamadriz/friendly-snippets",
+        },
+
+        config = function()
+            local lsp = require("lsp-zero")
+            lsp.preset("recommended")
+
+            lsp.set_preferences({
+                set_lsp_keymaps = true,
+                manage_nvim_cmp = true,
+            })
+
+            lsp.setup()
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+    },
+    {
+    "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+        keys = { { "<leader>pv", "<cmd>Neotree<cr>", desc = "Neotree" } },
+    },
+    {
+        "simrat39/symbols-outline.nvim",
+        cmd = "SymbolsOutline",
+        keys = { { "<leader>so", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+        config = true,
+    },
+})
