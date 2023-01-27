fc1ec5e0f1b07d8a93c6d6b50d9df12919cf7b4b
fjij <30779570+fjij@users.noreply.github.com>
2023-01-27 14:12:53 -0500

    Use opt instead of vim.opt

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,57 +1,59 @@
+local opt = vim.opt
+
 -- Line Numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
+opt.number = true
+opt.relativenumber = true
 
 -- Columns
-vim.opt.colorcolumn = "81"
-vim.opt.wrap = false
+opt.colorcolumn = "81"
+opt.wrap = false
 
 -- Tabs
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.autoindent = true
-vim.opt.expandtab = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.autoindent = true
+opt.expandtab = true
 
 -- Disable Modelines
-vim.opt.modelines = 0
-vim.opt.modeline = false
+opt.modelines = 0
+opt.modeline = false
 
 -- Disable hidden
-vim.opt.hidden = false
+opt.hidden = false
 
 -- Disable mouse
-vim.opt.mouse = ""
+opt.mouse = ""
 
 -- Backups
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
 
 -- Sign column
-vim.opt.signcolumn = "number"
+opt.signcolumn = "number"
 
 -- Search
-vim.opt.incsearch = true
+opt.incsearch = true
 
 -- Block cursor
-vim.opt.guicursor = ""
+opt.guicursor = ""
 
 -- Misc
-vim.opt.termguicolors = true
+opt.termguicolors = true
 
 -- Folding
 function _G.my_fold_text()
     local a = vim.fn.getline(vim.v.foldstart)
-    local b = vim.fn.getline(vim.v.foldend):gsub("%s+", "")
+    local b = vim.fn.getline(vim.v.foldend):gsub("^%s*(.-)%s*$", "%1")
     return a .. " ... " .. b
 end
 
-vim.opt.foldenable = false
-vim.opt.foldmethod = "syntax"
-vim.opt.foldlevelstart = 99
-vim.opt.foldtext = "v:lua.my_fold_text()"
-vim.opt.foldnestmax = 3
-vim.opt.foldminlines = 1
-vim.opt.fillchars = "fold: " 
+opt.foldenable = false
+opt.foldmethod = "syntax"
+opt.foldlevelstart = 99
+opt.foldtext = "v:lua.my_fold_text()"
+opt.foldnestmax = 3
+opt.foldminlines = 1
+opt.fillchars = "fold: "
