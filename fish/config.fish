f69a09735fe20ef1ebcf3303e0c8761ec9b0d2b1
fjij <30779570+fjij@users.noreply.github.com>
2023-03-08 14:38:23 -0500

    Add fish local config

--- fish/config.fish
+++ fish/config.fish
@@ -6,9 +6,40 @@ set -gx CLICOLOR 1
 set -gx fish_greeting
 fish_config prompt choose arrow
 fish_add_path ~/bin
+fish_add_path ~/av/tools
 if test -e /opt/homebrew/bin/brew
     eval (/opt/homebrew/bin/brew shellenv)
 end
 if test -e /home/linuxbrew/.linuxbrew/bin/brew
     eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
+
+fish_config prompt choose informative_vcs
+
+# Git
+abbr gs git status
+abbr gd git diff
+abbr gds git diff --staged
+abbr gb git branch
+abbr ga git add
+abbr gc git commit
+abbr gco git checkout
+abbr gp git push
+abbr gpu git push -u origin HEAD
+abbr gr git restore
+abbr gl git log
+abbr gpull git pull
+
+# misc
+abbr cls clear
+abbr issues gh issue list -a @me
+abbr pr gh pr
+function current_issue
+    git branch --show-current | grep -o -E '[0-9]+'
+end
+abbr ISSUE --position=anywhere -f current_issue
+
+# other
+if test -e ~/.config/fish/local_config.fish
+    source ~/.config/fish/local_config.fish
 end
