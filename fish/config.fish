fb1924c81518bdadeecbb8d61229a9466e4a36d9
fjij <30779570+fjij@users.noreply.github.com>
2023-09-06 11:15:01 -0400

    Remove unused path

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,6 @@ set -gx MANPAGER 'sh -c \'col -bx | bat -l man -p\''
 set -gx fish_greeting
 fish_config prompt choose arrow
 fish_add_path ~/bin
-fish_add_path ~/av/tools
 if test -e /opt/homebrew/bin/brew
     eval (/opt/homebrew/bin/brew shellenv)
 end
