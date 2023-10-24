17c54a62b6d0a0a2d53f4ef1bde4b4a479321042
fjij <30779570+fjij@users.noreply.github.com>
2023-10-24 15:27:20 -0400

    Add solidity = 4 spaces

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -37,7 +37,7 @@ augroup("spacing", {})
 
 autocmd("FileType", {
   group = "spacing",
-  pattern = { "python", "c", "cpp", "rust", "sql" },
+  pattern = { "python", "c", "cpp", "rust", "sql", "solidity" },
   callback = use_spaces(4),
 })
 
