1b98ff8224b3bc5809b6c3ef49cb4f5a5b8c1ef4
fjij <30779570+fjij@users.noreply.github.com>
2024-07-10 21:19:52 +0000

    Change completion to tab / shift tab

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -58,7 +58,7 @@ return {
               fallback()
             end
           end),
-          ["<Down>"] = cmp.mapping(function(fallback)
+          ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
             elseif luasnip.locally_jumpable(1) then
@@ -67,7 +67,7 @@ return {
               fallback()
             end
           end, { "i", "s" }),
-          ["<Up>"] = cmp.mapping(function(fallback)
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
             elseif luasnip.locally_jumpable(-1) then
