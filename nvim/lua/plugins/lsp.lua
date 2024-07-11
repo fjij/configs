636a2cf766b78b34efefa42d4dbdc56f9af87c5e
fjij <30779570+fjij@users.noreply.github.com>
2024-07-11 01:19:40 -0400

    No more tab please, I just don't like it

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -58,7 +58,7 @@ return {
               fallback()
             end
           end),
-          ["<Tab>"] = cmp.mapping(function(fallback)
+          ["<Down>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
             elseif luasnip.locally_jumpable(1) then
@@ -67,7 +67,7 @@ return {
               fallback()
             end
           end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
+          ["<Up>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
             elseif luasnip.locally_jumpable(-1) then
